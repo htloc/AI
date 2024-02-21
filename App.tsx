138bc5a2a494f5512ec74f928c6553313721ae8d
htloc <lochatan19@gmail.com>
2024-02-21 15:20:41 +0700

    init project

--- App.tsx
+++ App.tsx
@@ -5,114 +5,70 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect } from 'react';
+
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import { loadTensorflowModel, useTensorflowModel } from 'react-native-fast-tflite';
+import { Camera, Frame, useCameraDevice, useCameraPermission, useFrameProcessor } from 'react-native-vision-camera';
+import { useResizePlugin } from 'vision-camera-resize-plugin';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function App() {
+  const { resize } = useResizePlugin()
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const device = useCameraDevice('back')
+  const objectDetection = useTensorflowModel(require('./src/assets/models/mobilenet_v1_1.0_224_quant.tflite'))
+  const model = objectDetection.state === "loaded" ? objectDetection.model : undefined
+  // 640 x 640
+  useEffect(() => {
+    const fetch = async () => {
+      if (!hasPermission) {
+        await requestPermission();
+      }
+    }
+    fetch()
+  }, [])
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // User can look for specific objects
+const targetObject = 'banana'
+
+const frameProcessor = useFrameProcessor((frame) => {
+  'worklet'
+   
+  //console.log(frame.toString())
+
+  const data = resize(frame, {
+    scale: {
+      width: 640,
+      height: 640,
+    },
+    pixelFormat: 'rgb',
+    dataType: 'uint8'
+  })
+
+  const input = new Uint8Array(data)
+  const outputs = model?.runSync([input])
+  console.log(outputs?.toString())
+  
+  //const bananas = objects.filter((o) => o.type === targetObject)
+  //console.log(`Detected ${bananas} bananas!`)
+}, [])
+
+  if (device == null) return <View><Text>aa</Text></View>
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+      frameProcessor={frameProcessor}
+      
+    />
+  )
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
+
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,18 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
+const defaultConfig = getDefaultConfig();
+const { assetExts } = defaultConfig.resolver;
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    resolver: {
+        assetExts: [...assetExts, 'tflite']
+    }
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
